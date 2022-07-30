5b7225cd7c29bc9cdfc1978e9cedb9d2bd611fb6
jenniferve09 <105527570+jenniferve09@users.noreply.github.com>
2022-07-29 20:46:21 -0500

    Add files via upload

---
+++ Audifonos.cs
@@ -0,0 +1,12 @@
+﻿public class Audifonos  : Decorador
+{
+
+    public Audifonos (OrdenBase ordenBase) : base(ordenBase) { }
+
+    public override string Software()
+    {
+        var teclado = "Audifonos agregado";
+        
+        return teclado;
+    }
+}
